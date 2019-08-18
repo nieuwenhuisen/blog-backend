71ca3465112762277f402ae5b655ab7938ba18f3
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-08-18 12:42:01 +0200

    Add redux persists to persist the cms state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,18 @@
 import { combineReducers } from 'redux';
 
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import cmsReducer from './cms/cms.reducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cms']
+}
 
 const rootReducer = combineReducers({
     cms: cmsReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
